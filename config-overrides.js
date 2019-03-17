d934b4772b67c37906551e8ff871c00eae19942a
natgit8 <natasha.gitlin8@gmail.com>
2019-03-17 01:06:46 -0400

    added components and images as well as updated my webpack config file

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+};
