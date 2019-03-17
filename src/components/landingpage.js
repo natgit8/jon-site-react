7c2e1317823e5ea9bf665f7112505253927ababd
natgit8 <natasha.gitlin8@gmail.com>
2019-03-17 02:03:35 -0400

    adds landingpage styling

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,33 +1,12 @@
 import React, { Component } from 'react';
-import { Grid, Cell }from 'react-mdl';
+import { Grid, Cell, Button }from 'react-mdl';
 import img from '../images/sitephoto.jpg';
 import image from '../images/paintbrush.jpg';
-import { Button } from 'semantic-ui-react'; 
 
 class LandingPage extends Component {
   render() {
     return(
-
-      <div className="about">
-        <div id="about-bg-diagonal" className="bg-parallax"> </div>
         <Grid className="landing-grid">
-          <Cell className="col-4" col={4}>
-            <div id="about-content-box">
-              <div id="about-content-box-inner">
-                <div className="content-title">
-                  <h3>Create </h3>
-                  </div>
-              </div>
-              <div id="about-desc">
-                <p>
-                  Lorem ipsum wiley coyote
-                </p>
-              </div>
-              <div id="about-btn">
-
-              </div>
-            </div>
-          </Cell>
           <Cell className="col-4" col={4}>
             <img src={img}
                  alt="SitePhoto"
@@ -35,9 +14,7 @@ class LandingPage extends Component {
                  height="650px"
                  />
           </Cell>
-
         </Grid>
-      </div>
     )
   }
 }
